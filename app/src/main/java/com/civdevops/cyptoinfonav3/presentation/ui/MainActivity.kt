c2fd10eb2dfdbf0ea385d9c80992c27b18ff4624
J R C Madushanka <jrcmadushanka@gmail.com>
2026-08-06 04:25:05 +0530

    DI base

--- app/src/main/java/com/civdevops/cyptoinfonav3/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/civdevops/cyptoinfonav3/presentation/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.civdevops.cyptoinfonav3.presentation
+package com.civdevops.cyptoinfonav3.presentation.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.civdevops.cyptoinfonav3.presentation.ui.theme.CyptoInfoNav3Theme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
