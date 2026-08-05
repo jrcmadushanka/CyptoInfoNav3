f27c041c6629310a0156f5cea8fc9c5d2a97f376
J R C Madushanka <jrcmadushanka@gmail.com>
2026-08-06 03:46:46 +0530

    Initial commit

---
+++ app/src/main/java/com/civdevops/cyptoinfonav3/presentation/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.civdevops.cyptoinfonav3.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.civdevops.cyptoinfonav3.presentation.ui.theme.CyptoInfoNav3Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CyptoInfoNav3Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    CyptoInfoNav3Theme {
+        Greeting("Android")
+    }
+}
